f1cdddff38d316c140bb3a99f7b60b438725f8f0
alunhewinson <alunhewinson@gmail.com>
2021-01-03 18:53:29 +0000

    Complete Day 20 solution

--- 2015/a20.R
+++ 2015/a20.R
@@ -16,3 +16,13 @@ for (i in seq(801, length.out=100)) {#000) {
 i18a <- i*1000 + min(which(10 * sapply(seq(i*1000, length.out = 1000), Sigma) >= i18))-1
 glue("The answer to Part 1 is {i18a}") %>% message()
 
+## Part 2
+start <- 800001
+i18b <- which(
+  sapply(seq(start, length.out = 100000), function(i) {
+    divs <- i/(1:50)
+    sum(divs[divs %% 1 == 0])*11
+  }) > i18
+) + start - 1
+
+glue("The answer to Part 2 is {min(i18b)}") %>% message()
