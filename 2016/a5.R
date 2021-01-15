ae25420a38da110e5aad33625246b1f47a8f26f0
alunhewinson <alunhewinson@gmail.com>
2021-01-15 22:26:46 +0000

    update with MY answers for Day 5

--- 2016/a5.R
+++ 2016/a5.R
@@ -9,18 +9,21 @@ library(digest)
 i5 <- readLines("2016/inputs/i5.txt", warn = FALSE)
 #i5 <- "abc"
 
-base <- 24600000
+base <- 0
 finds <- 0
 repeat {
-  i5a <- paste0(i5, base + 0:999)
+  i5a <- paste0(i5, base + 0:9999)
   i5a %<>% sapply(digest, serialize=FALSE)
   fiveohs <- i5a %>% str_detect("^00000")
   if (any(fiveohs)) {
     print(i5a[fiveohs])
     finds <- finds + sum(fiveohs)
   }
-  if (finds >= 8) break
-  base <- base + 1000
+  if (finds >= 20) break
+  base <- base + 10000
 }
 
 ## This one's still a bit manual, just running it in blocks of 8 until you get the answers you need.
+## f2c730e5
+glue("The answer to Part 1 is {'d4cd2ee1'}") %>% message()
+glue("The answer to Part 2 is {'f2c730e5'}") %>% message()
